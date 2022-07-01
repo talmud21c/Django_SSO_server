ccfa247d73216119ea20bc3e448d88265ebfe42d
Jaeseok Park <m2363@naver.com>
2022-07-01 18:40:08 +0900

    init

---
+++ base/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import MyTokenObtainPairView
+
+urlpatterns = [
+    path('', views.getRoutes),
+    # simple JWT token
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
